c9bb6f48925437c271adb598c5b93258a789bc81
yuyaamano23 <nba.chako.nba@icloud.com>
2021-09-13 16:56:05 +0900

    nli bert-scoreの比較実験のベースとなるコード

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from bert_nli import BertNLIModel
 import pandas as pd
 from bertscore import calc_bert_score
 
+# csvデータセットを整形
 def load_dataset(filepath, encoding='utf-8'):
     df = pd.read_csv(filepath, encoding=encoding)
     df['t1']=df['t1'].replace("\'s", '')
@@ -15,37 +16,27 @@ def load_dataset(filepath, encoding='utf-8'):
         b.append(d2)
     for l in df['label']:
         c.append(l)
-    #for l in df['label']:
-        #if l=='not_entailment':
-        #    ll='contradiction'
-        #elif l=='entailment':
-        #    ll='entail'
-        #c.append(ll)
-
     return a,b,c
 sent1,sent2,labels=load_dataset('./ge_test.csv')
 
 acc=len(sent1)
+# nliの正解数
+pre1=0
 pre=0
+# bertscoreの正解数
 pre1=0
 #bert_type = 'bert-base'
 
+# ファインチューニング済のモデルを読み込む
 model = BertNLIModel('./nli_model_acc0.8831943861332694.state_dict')
 for s1,s2,l in zip(sent1,sent2,labels):
-    fla=''
-    if l in 'entail':
-        fla='contradiction'
-    else:
-        fla='entail'
+    # 以下nli
     sent_pairs = [(s1,s2)]
-    #print(sent_pairs)
     label,prob= model(sent_pairs)
-    print('正解：',l,'予測',label[0])
-    print('前提：',s1)
-    print('仮定：',s2)
-    print(' ')
+    print('正解：',l,'予測：',label[0])
     if l==label[0] or label[0] in 'neutral':
         pre+=1
+    # 以下bertscore
     P, R, F1 = calc_bert_score([s1], [s2])
     F1=F1[0]
     if F1>=0.85 and label[0] in 'entail':
