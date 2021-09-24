92e699ca21ed1ee694ca838700bb24bfeebc112e
yuyaamano23 <nba.chako.nba@icloud.com>
2021-09-25 00:04:12 +0900

    データ文にラベルを加えてnliの正解率を出した

--- test.py
+++ test.py
@@ -8,19 +8,18 @@ def load_dataset(filepath, encoding='utf-8'):
     df = pd.read_csv(filepath, encoding=encoding)
     # 意図せず\sが出てきてしまっていたので消す
     df['S']=df['S'].replace("\'s", '')
-    # df['答え']=df['答え'].replace("\'s", '')
+    df['L']=df['L'].replace("\'s", '')
     a=[]
     b=[]
-    # c=[]
+    c=[]
     for d1 in df['S']:
         a.append(d1)
     for d2 in df['T']:
         b.append(d2)
-        # c.append('contradiction')
-    return a,b
-    # return c
-sent1,sent2=load_dataset('./論文データ文.csv')
-# sent1,sent2,labels=load_dataset('./論文不正解文.csv')
+    for d3 in df['L']:
+        c.append(d3)
+    return a,b,c
+sent1,sent2,labels=load_dataset('./論文データ文.csv')
 
 # 問題数
 acc=len(sent1)
@@ -31,25 +30,30 @@ ans=0
 ans_bert_score=0
 #bert_type = 'bert-base'
 
-# 以下nli
+
 # ファインチューニング済のモデルを読み込む
 model = BertNLIModel('./nli_model_acc0.8831943861332694.state_dict')
-# for s1,s2,l in zip(sent1,sent2,labels):
-for s1,s2 in zip(sent1,sent2):
+for s1,s2,l in zip(sent1,sent2,labels):
+# ========================  以下nli  ================================
     # 両方向で含意関係認識
     sent_pairs_right = [(s1,s2)]
     sent_pairs_left = [(s2,s1)]
     nli_label_right,prob_right = model(sent_pairs_right)
     nli_label_left,prob_left = model(sent_pairs_left)
-    nli_label = 'contradiction'
+    nli_label = ''
     # contradiction entail neutralの順番で2次元配列に格納されている
     if prob_right[0][1].item() > 0.95 and prob_left[0][1].item() > 0.95:
         nli_label = 'entail'
-    print('【nli】','正解：contradiction','予測：',nli_label)
-    if nli_label == 'contradiction':
+    else:
+        nli_label = 'contradiction'
+    print('【nli】','正解：',l,'予測：',nli_label)
+    if nli_label == l:
         ans+=1
-    print('nli_pre：',ans)
-    # 以下bertscore
+        print('ansプラスされました!!')
+    else:
+        print('ansプラスされませんでした!')
+    print('nli：',ans)
+# ========================  以下bert_score  ===========================
     # P, R, F1 = calc_bert_score([s1], [s2])
     # # 予測
     # pred = 'contradiction or neutral'
@@ -69,14 +73,26 @@ for s1,s2 in zip(sent1,sent2):
     # print('bertscore_pre1',pre1)
     # print('======================================================')
 
+
+
+# =========================== 以下データ算出 =================================
+# 1文字目：T(True)は予測正解、F(False)は予測不正解。
+# 2文字目：Pは予測が正(Positive)、Nは予測が負(Negative)
+# https://qiita.com/FukuharaYohei/items/be89a99c53586fa4e2e4#%E6%B7%B7%E5%90%88%E8%A1%8C%E5%88%97confusion-matrix%E3%81%A8tp-fp-fn-tn
+# 適合率...正と予測したデータのうち、実際に正であった確率 「精度」と呼ぶこともあり。
+# 再現率...実際に正であるもののうち、正であると予測された確率 私は心の中では「回収率」と呼んでいます(車などのリコール(回収)と同じ)。実際に正であるものをどれだけ回収(予測)できたかの割合
+# F値... 2∗適合率∗再現率 適合率+再現率
+
+# 分母は114 37
 # 適合率の算出
-# def calc_precicsion(tp,acc):
-    # pre = /ans
+def calc_precicsion(tp,fp):
+    pre = tp / tp + fp
+    return pre
 
 # 再現率の算出
-# def calc_recall(tp,acc):
-#     rec = tp/acc
-#     return rec
+def calc_recall(tp,fn):
+    rec = tp / tp + fn
+    return rec
 
 
 
